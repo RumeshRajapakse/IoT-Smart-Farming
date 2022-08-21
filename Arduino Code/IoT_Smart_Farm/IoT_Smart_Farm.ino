4f8fd0d562d02a43ac3751d22915cac60c60ca12
Rumesh Rajapakse <86800093+RumeshRajapakse@users.noreply.github.com>
2022-08-21 19:41:15 +0100

    read from temperature sensors

--- Arduino Code/IoT_Smart_Farm/IoT_Smart_Farm.ino
+++ Arduino Code/IoT_Smart_Farm/IoT_Smart_Farm.ino
@@ -1,23 +1,44 @@
+#include <LiquidCrystal.h>
+
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include "DHT.h"
+LiquidCrystal_I2C lcd(0x27,20,4);
+#define ENVTEMP A0  // read the environmental temperature and humidity
+#define WATTEMP A1  // read the water temperature
+#define DHT1TYPE DHT11   // DHT 11 
+#define DHT2TYPE DHT11   // DHT 11 
+
+DHT dht1(ENVTEMP, DHT1TYPE);
+DHT dht2(WATTEMP, DHT2TYPE);
+
 void setup() {
 Serial.begin(9600);
+  lcd.init();  
+  lcd.backlight();
+  lcd.init();
+  dht1.begin();
+  dht2.begin();
 }
  
 void loop() {
-  //Let's generate our random data
- int t = random(20,32);
- int h = random(60,100);
- int rnd = random(1000);
+  //reading temperature and humidity from sensor
+ float h1 = dht1.readHumidity();
+ float t1 = dht1.readTemperature();
+ float h2 = dht2.readHumidity();
+ float t2 = dht2.readTemperature();
  
-  //Note that it won't send it to the pi without the ln in Serial.println
- Serial.print("t=");
- Serial.println(t);
  
- Serial.print("h=");
- Serial.println(h);
+  //printing the sensor values through serial printer
  
- Serial.print("random=");
- Serial.println(rnd);
- 
- //Wait 2.5 seconds and start the party over again
- delay(2500);
+ Serial.print("tout=");
+ Serial.println(t1);
+ Serial.print("hout=");
+ Serial.println(h1);
+ Serial.print("tin=");
+ Serial.println(t2);
+ Serial.print("hin=");
+ Serial.println(h2);
+
+ delay(2500); // delay between reading each sensor data
 }
